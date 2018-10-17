5183a607dcdf305fa9751da7dad72d537dcee364
paulohgama <santosps1990@gmail.com>
2018-10-17 17:37:50 -0300

    Form inscrições parcialmente pronto

--- wp-content/themes/TemaPrincipal/form-inscricao.php
+++ wp-content/themes/TemaPrincipal/form-inscricao.php
@@ -6,19 +6,75 @@ Template Name: Formulario de Inscrição
 <?php get_header(); ?>
 <div class="container">
 	<form class="form-horizontal" method="post">
-		<div class="form-nome">
-			<label for="form-nome">Nome:</label>
-			<input id="form-nome" type="text" placeholder="Seu nome aqui" name="form-nome">
+		<div class="form-group">
+                    <label class="label-control col-sm-2" for="nome">Nome:</label>
+                    <div class="col-sm-10">
+                        <input id="nome" class="form-control" type="text" placeholder="Seu nome completo aqui" name="nome">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="label-control col-sm-2" for="data">Data:</label>
+                    <div class="col-sm-10">
+                        <input id="data" class="form-control" type="text" placeholder="Sua data de nascimento aqui" name="data">
+                    </div>
 		</div>
-		<div class="form-email">
-			<label for="form-email">Email:</label>
-			<input id="form-email" type="email" placeholder="email@exemplo.com.br" name="form-email">
+                <div class="form-group">
+                    <label class="label-control col-sm-2" for="cpf">CPF:</label>
+                    <div class="col-sm-10">
+                        <input id="cpf" class="form-control" type="text" placeholder="Insira seu cpf aqui" name="cpf">
+                    </div>
 		</div>
-		<div class="form-mensagem">
-			<label for="form-mensagem">Mensagem:</label>
-			<textarea id="form-mensagem" name="form-mensagem"></textarea>
+                <div class="form-group">
+                    <label class="label-control col-sm-2" for="email">Email:</label>
+                    <div class="col-sm-10">
+                        <input id="form-email" class="form-control" type="email" placeholder="email@exemplo.com.br" name="email">
+                    </div>
 		</div>
-		<button type="submit">Enviar</button>
+                <div class="form-group">
+                    <label class="label-control col-sm-2" for="cep">CEP:</label>
+                    <div class="col-sm-10">
+                        <input id="rg" class="form-control" type="text" placeholder="Insira seu cep aqui" name="cep">
+                    </div>
+		</div>
+                <div class="form-group">
+                    <label class="label-control col-sm-2" for="endereco">Endereço:</label>
+                    <div class="col-sm-10">
+                        <input id="endereco" class="form-control" type="" placeholder="Seu endereço aqui" name="endereco">
+                    </div>
+		</div>
+                <div class="form-group">
+                    <label class="label-control col-sm-2" for="bairro">Bairro:</label>
+                    <div class="col-sm-10">
+                        <input id="bairro" class="form-control" type="text" placeholder="Seu bairro aqui" name="bairro">
+                    </div>
+		</div>
+                <div class="form-group">
+                    <label class="label-control col-sm-2" for="cidade">Cidade:</label>
+                    <div class="col-sm-10">
+                        <input id="cidade" class="form-control" type="text" placeholder="Sua cidade aqui" name="cidade">
+                    </div>
+		</div>
+                <div class="form-group">
+                    <label class="label-control col-sm-2" for="estado">Estado:</label>
+                    <div class="col-sm-10">
+                        <input id="estado" class="form-control" type="text" placeholder="Seu estado aqui" name="estado">
+                    </div>
+		</div>
+                <div class="form-group">
+                    <label class="label-control col-sm-2" for="telefone">Telefone:</label>   
+                    <div class="col-sm-10">
+                        <input id="telefone" class="form-control" type="tel" placeholder="Seu telefone aqui" name="telefone">
+                    </div>
+		</div>
+                <div class="form-group">
+                    <label class="label-control col-sm-2" for="celular">Celular:</label>   
+                    <div class="col-sm-10">
+                        <input id="celular" class="form-control" type="text" placeholder="Seu celular aqui" name="celular">
+                    </div>
+		</div>
+                <div class="col-sm-offset-2 col-sm-10" style="margin-left: 180px">
+                    <button class="btn btn-success" type="submit">Enviar</button>
+                </div>
 	</form>
 </div>
 <?php get_footer(); ?>
--- wp-content/themes/TemaPrincipal/header.php
+++ wp-content/themes/TemaPrincipal/header.php
@@ -7,11 +7,10 @@
 		<link rel="stylesheet" href="<?= $home ?>/header.css">
 		<link rel="stylesheet" href="<?= $home ?>/reset.css">
 		<link rel="stylesheet" href="<?= $home ?>/<?= $css_especifico ?>.css">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-		<meta charset="utf-8">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+                <meta charset="utf-8">
 		<title><?php bloginfo('name');
 		if (!is_home()) {
 			echo ' | ';
@@ -19,11 +18,14 @@
 		}?></title>
 	</head>
 	<body>
-		<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-			<a class="navbar-brand" href="<?=get_site_url()?>">Pagina Inicial</a>
-			<ul class="navbar-nav">
-			    <li class="nav-item">
-			      <a class="nav-link" href="<?=get_site_url().'/cursos/'?>">Cursos</a>
-			    </li>
+		<nav class="navbar navbar-inverse">
+                    <div class="container-fluid">
+                        <div class="navbar-head">
+                            <a class="navbar-brand" href="#">Treinamentos Wordpress</a>
+                        </div>
+			<ul class="nav navbar-nav">
+                            <li><a href="<?=get_site_url()?>">Pagina Inicial</a> </li>
+                            <li><a class="nav-link" href="<?=get_site_url().'/cursos/'?>">Cursos</a></li>
 			</ul>
+                    </div>
 		</nav>
